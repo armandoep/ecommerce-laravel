9bd7a742729b798b95fec0fac5a90fc4dd8c958f
armandoep <60561840+armandoep@users.noreply.github.com>
2021-05-26 21:06:15 -0600

    Pequeños cambios en el dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,20 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <h1 class="my-5 mx-3">Bienvenido {{ auth()->user()->name }} </h1>
+                <hr />
 
+                <h2 class="my-5 mx-3">Estos son los productos disponibles en la tienda: </h2>
+
+                @livewire('products-table')
+            </div>
+        </div>
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <h1 class="my-5 mx-3">Ultimas ventas </h1>
+                <p class="my-5 mx-3">Trabajando... </p>
+                <hr />
             </div>
         </div>
     </div>
